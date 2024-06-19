84321ca45424e7c399684635e261d5c164fb5f0c
Alice-K-github <157291081+Alice-K-github@users.noreply.github.com>
2024-06-19 14:38:58 +0300

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,6 +2,13 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+select customers.company_name as customer, employees.first_name || ' ' || employees.last_name as employee from orders
+inner join customers using(customer_id)
+inner join employees using(employee_id)
+inner JOIN shippers ON orders.ship_via = shippers.shipper_id
+where 'United Package' in (shippers.company_name)
+and 'London' in (customers.city)
+and 'London' in (employees.city)
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
